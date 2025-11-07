20d746dc899b88a64aea987ee8e91e0f90107fe9
Muazzam Younus - RI <Muazzam Younus - RI>
2025-11-07 16:02:33 +0500

    Package update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://table-tennis-backend.vercel.app',
         changeOrigin: true
       }
     }
